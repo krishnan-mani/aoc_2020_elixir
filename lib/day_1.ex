fbdf91abe09a0b389550459d1596d103b3516b6d
Krishnan M <km@krishnanm.com>
2021-07-05 21:39:43 +0100

    implemented complement on number or list

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -2,12 +2,26 @@ defmodule Day1 do
 
   @doc """
   Finds the additive complement of the number and sum
-
   ## Examples
-  #
     iex> Day1.complement(3, 10)
     7
+    iex> Day1.complement([3, 7], 10)
+    {3, 7}
+    iex> Day1.complement([3, 4, 5, 6], 9)
+    {3, 6}
+    iex> Day1.complement([3, 8], 10)
+    {}
   """
+  def complement([], _sum) do {} end
+  def complement([head | tail], sum) do
+    head_complement = complement(head, sum)
+    if Enum.member?(tail, head_complement) do
+      {head, head_complement}
+    else
+      complement(tail, sum)
+    end
+  end
+
   def complement(n, sum) do sum - n end
 
 end
