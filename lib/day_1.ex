17b46418726e9afcc6fcba43a2fd3d8a3a2d88ef
Krishnan M <km@krishnanm.com>
2021-07-05 21:15:17 +0100

    started on day 1

---
+++ lib/day_1.ex
@@ -0,0 +1,13 @@
+defmodule Day1 do
+
+  @doc """
+  Finds the additive complement of the number and sum
+
+  ## Examples
+  #
+    iex> Day1.complement(3, 10)
+    7
+  """
+  def complement(n, sum) do sum - n end
+
+end
