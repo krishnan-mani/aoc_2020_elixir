04f66f4d6314c1cd3626e793e206ea42ece358f8
Krishnan M <km@krishnanm.com>
2021-07-06 22:57:56 +0100

    solved day 2, part 2

--- day_2.exs
+++ day_2.exs
@@ -5,3 +5,10 @@ password_lines =
   |> Enum.map(&Day2.parse_line/1)
 count = Enum.count(password_lines, fn(x) -> Day2.valid_password?(x.min, x.max, x.chr, x.password) end)
 IO.puts "Number of valid passwords: #{count}"
+
+password_lines_part2 = 
+  stream
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&Day2Part2.parse_line/1)
+count = Enum.count(password_lines_part2, fn(x) -> Day2Part2.valid_password?(x.password, x.chr, x.pos1, x.pos2) end)
+IO.puts "Per toboggan policy, number of valid passwords: #{count}"
--- lib/day_2p2.ex
+++ lib/day_2p2.ex
@@ -25,16 +25,16 @@ defmodule Day2Part2 do
 
   ##Examples
   #
-    iex> Day2Part2.password_valid?("abcde", 'a', 1, 3)
+    iex> Day2Part2.valid_password?("abcde", 'a', 1, 3)
     true
-    iex> Day2Part2.password_valid?("cdefg", 'b', 1, 3)
+    iex> Day2Part2.valid_password?("cdefg", 'b', 1, 3)
     false
-    iex> Day2Part2.password_valid?("ccccccccc", 'c', 2, 9)
+    iex> Day2Part2.valid_password?("ccccccccc", 'c', 2, 9)
     false
   """
-  def password_valid?(str, chr, pos1, pos2) do
-    present_at_pos1 = String.at(str, pos1 - 1) |> String.to_charlist == chr 
-    present_at_pos2 = String.at(str, pos2 - 1) |> String.to_charlist == chr
+  def valid_password?(password, chr, pos1, pos2) do
+    present_at_pos1 = String.at(password, pos1 - 1) |> String.to_charlist == chr 
+    present_at_pos2 = String.at(password, pos2 - 1) |> String.to_charlist == chr
     case [present_at_pos1, present_at_pos2] do
       [true, true] -> false
       [false, false] -> false
