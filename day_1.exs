9f8be1d8a4180d5db6968f392f0f8eed2555036e
Krishnan M <km@krishnanm.com>
2021-07-06 21:47:02 +0100

    reformatting

--- day_1.exs
+++ day_1.exs
@@ -1,6 +1,9 @@
 sum = 2020
 stream = File.stream!("input/day1.txt")
-numbers = stream |> Enum.map(&String.trim/1) |> Enum.map(&String.to_integer/1)
+numbers = 
+  stream 
+  |> Enum.map(&String.trim/1) 
+  |> Enum.map(&String.to_integer/1)
 {a, b} = Day1.complement(numbers, sum)
 IO.puts "day 1, part 1: product of two numbers that sum to 2020: #{a * b}"
 
