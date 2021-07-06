cd486ad971aa4416f5442fa85375335b19a84209
Krishnan M <km@krishnanm.com>
2021-07-06 22:49:42 +0100

    implemented password validation for part 2

--- lib/day_2p2.ex
+++ lib/day_2p2.ex
@@ -18,4 +18,28 @@ defmodule Day2Part2 do
       password: password
     }
   end
+
+
+  @doc """
+  Password is valid only if char is present at one of the positions
+
+  ##Examples
+  #
+    iex> Day2Part2.password_valid?("abcde", 'a', 1, 3)
+    true
+    iex> Day2Part2.password_valid?("cdefg", 'b', 1, 3)
+    false
+    iex> Day2Part2.password_valid?("ccccccccc", 'c', 2, 9)
+    false
+  """
+  def password_valid?(str, chr, pos1, pos2) do
+    present_at_pos1 = String.at(str, pos1 - 1) |> String.to_charlist == chr 
+    present_at_pos2 = String.at(str, pos2 - 1) |> String.to_charlist == chr
+    case [present_at_pos1, present_at_pos2] do
+      [true, true] -> false
+      [false, false] -> false
+      _ -> true
+    end
+  end
+
 end
