3458139ce5adc285249d614fd260516e70c6dc28
Krishnan M <km@krishnanm.com>
2021-07-07 15:10:00 +0100

    some refactoring

--- lib/day_2p2.ex
+++ lib/day_2p2.ex
@@ -9,11 +9,11 @@ defmodule Day2Part2 do
     %{pos1: 1, pos2: 3, chr: 'a', password: "abcde"}
   """
   def parse_line(str) do
-    [pos1, tail] = String.split(str, "-")
-    [pos2, chr, password] = String.split(tail, " ")
+    [position, tail] = String.split(str, "-")
+    [alt_position, chr, password] = String.split(tail, " ")
     %{
-      pos1: pos1 |> String.to_integer,
-      pos2: pos2 |> String.to_integer,
+      pos1: position |> String.to_integer,
+      pos2: alt_position |> String.to_integer,
       chr: chr |> String.at(0) |> String.to_charlist,
       password: password
     }
