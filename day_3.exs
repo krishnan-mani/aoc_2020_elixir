f0bb9db72c425d139c4b2090693d4bbf1954bd81
Krishnan M <km@krishnanm.com>
2021-07-08 14:54:06 +0100

    completed day 3

--- day_3.exs
+++ day_3.exs
@@ -1,4 +1,15 @@
 stream = File.stream!("input/day3.txt")
-grid = stream |> Enum.map(&String.trim/1)
+grid = stream
+       |> Enum.map(&String.trim/1)
 slope = %{right: 3, down: 1}
 IO.puts("Number of trees encountered (right 3, down 1): #{Day3.count_trees(grid, slope)}")
+
+slopes = [
+  %{right: 1, down: 1},
+  %{right: 3, down: 1},
+  %{right: 5, down: 1},
+  %{right: 7, down: 1},
+  %{right: 1, down: 2}
+]
+tree_counts = Enum.map(slopes, fn slope -> Day3.count_trees(grid, slope) end)
+IO.puts("Product of tree counts for different slopes: #{Enum.product(tree_counts)}")
