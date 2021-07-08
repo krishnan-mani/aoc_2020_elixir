7f7a80159ea7ead2b3fccb76200722a9ed019765
Krishnan M <km@krishnanm.com>
2021-07-08 14:28:48 +0100

    tests for different slopes, only advancing down one at a time

--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -21,13 +21,16 @@ defmodule Day3Test do
       {:ok, %{grid: grid}}
     end
 
-    test_with_params "following different slopes; one counts the expected number of trees", context,
+    test_with_params "following different slopes; one counts the expected number of trees, advancing only one row at a time", context,
                      fn (grid, slope, expected) ->
                        assert Day3.count_trees(grid, slope) == expected
                      end
       do
       [
-        {context.grid, %{right: 3, down: 1}, 7}
+        {context.grid, %{right: 1, down: 1}, 2},
+        {context.grid, %{right: 3, down: 1}, 7},
+        {context.grid, %{right: 5, down: 1}, 3},
+        {context.grid, %{right: 7, down: 1}, 4},
       ]
     end
   end
