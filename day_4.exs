4ee1ea8831c971abef5a985399ff43fab7b4f2d5
Krishnan Mani <km@krishnanm.com>
2021-07-09 21:32:38 +0100

    solved Day 4, part 1

---
+++ day_4.exs
@@ -0,0 +1,5 @@
+all_lines = File.read!("input/day4.txt")
+all_password_lines = String.split(all_lines, "\n\n")
+passwords = all_password_lines |> Enum.map(&Day4.parse_password/1)
+valid_password_count = Enum.count(passwords, fn x -> Day4.valid_password?(x)  end)
+IO.puts("Number of valid passwords: #{valid_password_count}")
