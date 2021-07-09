4ff2cc9e415daae33d71df2847e7331755027081
Krishnan Mani <km@krishnanm.com>
2021-07-09 20:43:51 +0100

    implemented password validation

--- lib/day4.ex
+++ lib/day4.ex
@@ -21,18 +21,33 @@ defmodule Day4 do
 
   @doc """
   Validates password
-  iex> first_valid_password_entry = Day4.parse_password("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+  iex> valid_first_password_entry = Day4.parse_password("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
   ...> byr:1937 iyr:2017 cid:147 hgt:183cm")
-  iex> Day4.valid_password?(first_valid_password_entry)
+  iex> Day4.valid_password?(valid_first_password_entry)
   true
-  iex> second_invalid_password_entry = Day4.parse_password("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+  iex> invalid_second_password_entry = Day4.parse_password("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
   ...> hcl:#cfa07d byr:1929")
-  iex> Day4.valid_password?(second_invalid_password_entry)
+  iex> Day4.valid_password?(invalid_second_password_entry)
+  false
+  iex> valid_third_password_entry = Day4.parse_password("hcl:#ae17e1 iyr:2013
+  ...> eyr:2024
+  ...> ecl:brn pid:760753108 byr:1931
+  ...> hgt:179cm")
+  iex> Day4.valid_password?(valid_third_password_entry)
+  true
+  iex> invalid_fourth_password_entry = Day4.parse_password("hcl:#cfa07d eyr:2025 pid:166559648
+  ...> iyr:2011 ecl:brn hgt:59in")
+  iex> Day4.valid_password?(invalid_fourth_password_entry)
   false
 """
   def valid_password?(password) do
-    Enum.all?([:eyr, :iyr, :byr, :hgt, :hcl, :ecl, :pid, :cid],
-      fn field -> Map.has_key?(password, field) end)
+    missing_required_fields = Enum.filter([:eyr, :iyr, :byr, :hgt, :hcl, :ecl, :pid, :cid],
+      fn field -> !Map.has_key?(password, field) end)
+    case missing_required_fields do
+      [] -> true
+      [:cid] -> true
+      _ -> false
+    end
   end
 
 end
