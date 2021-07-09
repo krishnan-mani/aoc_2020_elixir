a804d2bb36ecc1550c97e872991f03cd285f57ff
Krishnan Mani <km@krishnanm.com>
2021-07-09 21:44:00 +0100

    reformatting

--- day_2.exs
+++ day_2.exs
@@ -1,14 +1,14 @@
 stream = File.stream!("input/day2.txt")
-password_lines = 
-  stream 
-  |> Enum.map(&String.trim/1) 
+password_lines =
+  stream
+  |> Enum.map(&String.trim/1)
   |> Enum.map(&Day2.parse_line/1)
-count = Enum.count(password_lines, fn(x) -> Day2.valid_password?(x.min, x.max, x.chr, x.password) end)
+count = Enum.count(password_lines, fn (x) -> Day2.valid_password?(x.min, x.max, x.chr, x.password) end)
 IO.puts "Number of valid passwords: #{count}"
 
-password_lines_part2 = 
+password_lines_part2 =
   stream
   |> Enum.map(&String.trim/1)
   |> Enum.map(&Day2Part2.parse_line/1)
-count = Enum.count(password_lines_part2, fn(x) -> Day2Part2.valid_password?(x.password, x.chr, x.pos1, x.pos2) end)
+count = Enum.count(password_lines_part2, fn (x) -> Day2Part2.valid_password?(x.password, x.chr, x.pos1, x.pos2) end)
 IO.puts "Per toboggan policy, number of valid passwords: #{count}"
--- day_4.exs
+++ day_4.exs
@@ -1,5 +1,6 @@
 all_lines = File.read!("input/day4.txt")
 all_password_lines = String.split(all_lines, "\n\n")
-passwords = all_password_lines |> Enum.map(&Day4.parse_password/1)
+passwords = all_password_lines
+            |> Enum.map(&Day4.parse_password/1)
 valid_password_count = Enum.count(passwords, fn x -> Day4.valid_password?(x)  end)
 IO.puts("Number of valid passwords: #{valid_password_count}")
