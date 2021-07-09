9bae0a0a11c22afa9ddc0c59154edb83f0efc59a
Krishnan Mani <km@krishnanm.com>
2021-07-09 21:32:21 +0100

    moved doctest to test cases on account of iex issue with newlines

--- lib/day4.ex
+++ lib/day4.ex
@@ -1,14 +1,14 @@
 defmodule Day4 do
 
   @doc """
-  Parses password string into representation as fields and values
-
-    iex> Day4.parse_password("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-    ...> byr:1937 iyr:2017 cid:147 hgt:183cm")
-    %{ecl: "gry", pid: "860033327", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: "147", hgt: "183cm"}
-  """
+  Parses password line broken over multiple lines
+"""
   def parse_password(str) do
-    elements = str |> String.split(" ") |> Enum.map(&String.trim/1)
+    elements = str
+               |> String.split(" ")
+               |> Enum.map(fn x -> String.split(x, "\n") end)
+               |> List.flatten
+               |> Enum.map(&String.trim/1)
     Map.new(Enum.map(elements, fn x ->
       [key_str, value_str] = String.split(x, ":")
       value = case String.ends_with?(key_str, "yr") do
@@ -21,25 +21,10 @@ defmodule Day4 do
 
   @doc """
   Validates password
-  iex> valid_first_password_entry = Day4.parse_password("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-  ...> byr:1937 iyr:2017 cid:147 hgt:183cm")
-  iex> Day4.valid_password?(valid_first_password_entry)
-  true
-  iex> invalid_second_password_entry = Day4.parse_password("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-  ...> hcl:#cfa07d byr:1929")
-  iex> Day4.valid_password?(invalid_second_password_entry)
-  false
-  iex> valid_third_password_entry = Day4.parse_password("hcl:#ae17e1 iyr:2013
-  ...> eyr:2024
-  ...> ecl:brn pid:760753108 byr:1931
-  ...> hgt:179cm")
-  iex> Day4.valid_password?(valid_third_password_entry)
-  true
-  iex> invalid_fourth_password_entry = Day4.parse_password("hcl:#cfa07d eyr:2025 pid:166559648
-  ...> iyr:2011 ecl:brn hgt:59in")
-  iex> Day4.valid_password?(invalid_fourth_password_entry)
-  false
-"""
+    iex> valid_password = %{ecl: "gry", pid: "860033327", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: "147", hgt: "183cm"}
+    iex> Day4.valid_password?(valid_password)
+    true
+  """
   def valid_password?(password) do
     missing_required_fields = Enum.filter([:eyr, :iyr, :byr, :hgt, :hcl, :ecl, :pid, :cid],
       fn field -> !Map.has_key?(password, field) end)
