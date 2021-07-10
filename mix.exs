a0269ad832232d69236772a9d2513f7eaa7fe112
Krishnan Mani <km@krishnanm.com>
2021-07-10 14:21:32 +0100

    added excoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,15 @@ defmodule Aoc2020Elixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -22,7 +31,8 @@ defmodule Aoc2020Elixir.MixProject do
   defp deps do
     [
       {:mix_test_interactive, "~> 1.0", only: :dev, runtime: false},
-      {:ex_parameterized, "~> 1.3.7"}
+      {:ex_parameterized, "~> 1.3.7"},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 
