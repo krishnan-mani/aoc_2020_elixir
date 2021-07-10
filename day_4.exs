19e7fab56ba76a93a13dacc6de1770c1846441d6
Krishnan Mani <km@krishnanm.com>
2021-07-10 13:10:23 +0100

    Day 4 completed

--- day_4.exs
+++ day_4.exs
@@ -4,3 +4,6 @@ passports = all_passport_lines
             |> Enum.map(&Day4.parse_passport/1)
 valid_passport_count = Enum.count(passports, fn x -> Day4.valid_passport?(x)  end)
 IO.puts("Number of valid passports: #{valid_passport_count}")
+
+fully_valid_passport_count = Enum.count(passports, fn x -> Day4.fully_valid_passport?(x) end)
+IO.puts("Number of fully valid passports: #{fully_valid_passport_count}")
