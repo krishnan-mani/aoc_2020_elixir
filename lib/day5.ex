053290f219b9e74334b02c14e6abee1b89148197
Krishnan Mani <km@krishnanm.com>
2021-07-11 14:19:16 +0100

    refactoring

--- lib/day5.ex
+++ lib/day5.ex
@@ -1,30 +1,30 @@
 defmodule Day5 do
 
   @doc """
-  Bisect a range and return the upper or lower half
+  Bisect a range and return the greater or lesser half
     iex> range = 0..127
-    iex> range = range |> Day5.bisect(:lower)
+    iex> range = range |> Day5.bisect(:lesser)
     0..63
-    iex> range = range |> Day5.bisect(:upper)
+    iex> range = range |> Day5.bisect(:greater)
     32..63
-    iex> range = range |> Day5.bisect(:lower)
+    iex> range = range |> Day5.bisect(:lesser)
     32..47
-    iex> range = range |> Day5.bisect(:upper)
+    iex> range = range |> Day5.bisect(:greater)
     40..47
-    iex> range = range |> Day5.bisect(:upper)
+    iex> range = range |> Day5.bisect(:greater)
     44..47
-    iex> range = range |> Day5.bisect(:lower)
+    iex> range = range |> Day5.bisect(:lesser)
     44..45
-    iex> lower_bound = range |> Day5.bisect(:lower)
+    iex> lesser_bound = range |> Day5.bisect(:lesser)
     44..44
-    iex> upper_bound = range |> Day5.bisect(:upper)
+    iex> greater_bound = range |> Day5.bisect(:greater)
     45..45
   """
   def bisect(first..last = range, half) do
     midpoint = first + div(last - first, 2)
     case half do
-      :upper -> midpoint + 1..last
-      :lower -> first..midpoint
+      :greater -> (midpoint + 1)..last
+      :lesser -> first..midpoint
     end
   end
 
@@ -46,27 +46,21 @@ defmodule Day5 do
     iex> Day5.column('RLR')
     5
   """
-  def column(column_charlist) do
-    range = 0..(Integer.pow(2, Enum.count(column_charlist)) - 1)
-    Enum.reduce(column_charlist, range, fn selection, range ->
-      range = case selection do
-        ?R -> range |> bisect(:upper)
-        ?L -> range |> bisect(:lower)
-      end
-    end).first
-  end
+  def column(column_charlist) do converge(column_charlist, ?R, ?L) end
 
   @doc """
   Get row number
     iex> Day5.row('FBFBBFF')
     44
   """
-  def row(row_charlist) do
-    range = 0..(Integer.pow(2, Enum.count(row_charlist)) - 1)
-    Enum.reduce(row_charlist, range, fn selection, range ->
+  def row(row_charlist) do converge(row_charlist, ?B, ?F) end
+
+  defp converge(chrlist, greater_selector, lesser_selector) do
+    range = 0..(Integer.pow(2, Enum.count(chrlist)) -1)
+    Enum.reduce(chrlist, range, fn selection, range ->
       range = case selection do
-        ?F -> range |> bisect(:lower)
-        ?B -> range |> bisect(:upper)
+        ^greater_selector -> range |> bisect(:greater)
+        ^lesser_selector -> range |> bisect(:lesser)
       end
     end).first
   end
