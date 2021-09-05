c8abc0ce565c17fa21bb638dd9eebe3938d01abe
Krishnan Mani <km@krishnanm.com>
2021-09-05 21:48:18 +0100

    satisfied test case provided in problem description

--- test/day8_test.exs
+++ test/day8_test.exs
@@ -3,4 +3,25 @@ defmodule Day8Test do
   doctest Day8
 
 
+  describe "process instructions and detect loop" do
+    test "returns accumulator value before loop" do
+      instruction_strings = """
+      nop +0
+      acc +1
+      jmp +4
+      acc +3
+      jmp -3
+      acc -99
+      acc +1
+      jmp -4
+      acc +6
+      """
+      instructions = instruction_strings
+                     |> String.split("\n")
+                     |> Stream.map(&String.trim/1)
+                     |> Stream.reject(fn str -> String.length(str) == 0 end)
+                     |> Enum.map(&Day8.parse_instruction/1)
+      assert 5 == Day8.process_instructions(instructions)
+    end
+  end
 end
